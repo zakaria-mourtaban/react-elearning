408ccfb603a5d46fa30daa1441a34a577a10fdff
zakaria-mourtaban <zakariamourtaban@gmail.com>
2024-11-20 13:47:14 +0200

    added option request handling

--- server/connection.php
+++ server/connection.php
@@ -1,9 +1,9 @@
 <?php
 
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');  
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Origin: http://localhost:3000');  
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 $host = "localhost";
 $dbuser = "root";
@@ -17,3 +17,8 @@ if ($connection->connect_error) {
 }
 
 $data = json_decode(file_get_contents('php://input'), true);
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
