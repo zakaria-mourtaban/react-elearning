7ab44b3da8f0e2ac310730fbcbba9f0fe720186e
zakaria-mourtaban <zakariamourtaban@gmail.com>
2024-11-21 02:06:06 +0200

    fixed bug in courses page

--- src/pages/courses.jsx
+++ src/pages/courses.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 import "../styles/courses.css";
+import { useNavigate } from "react-router-dom";
 
 const Courses = () => {
 	const [courses, setCourses] = useState([]);
@@ -36,6 +37,7 @@ const Courses = () => {
 				setInvites(JSON.parse(res.data.invites));
 			});
 	};
+	const navigate = useNavigate();
 	useEffect(() => {
 		fetchcourses();
 		fetchinvites();
@@ -43,148 +45,151 @@ const Courses = () => {
 	}, []);
 	return (
 		<div className="courses-container">
-				<div className="courses">
-					<h1>available courses</h1>
-					{courses.map((course) => (
-						<div key={course.id} className="course">
-							<div className="CourseInfo">
-								<div className="icon">
-									<h3>{course.name[0]}</h3>
-								</div>
-								<h2>{course.name}</h2>
+			<div className="courses">
+				<h1>available courses</h1>
+				{courses.map((course) => (
+					<div key={course.id} className="course">
+						<div className="CourseInfo">
+							<div className="icon">
+								<h3>{course.name[0]}</h3>
 							</div>
-							<div className="ButtonsDiv">
-								<button
-									onClick={() => {
-										axios
-											.post(
-												"http://localhost/react-elearning/server/enrollcourse.php",
-												{
-													jwt: localStorage.getItem(
-														"jwt"
-													),
-													course_id: course.course_id,
-												},
-												{
-													headers: {
-														"Content-Type":
-															"application/json",
-													},
-												}
-											)
-											.then((res) => {
-												alert(res.data.message);
-											});
-									}}
-									className="EnrollBtn"
-								>
-									Enroll
-								</button>
-								<button
-									onClick={() => {
-										axios
-											.post(
-												"http://localhost/react-elearning/server/dropcourse.php",
-												{
-													jwt: localStorage.getItem(
-														"jwt"
-													),
-													course_id: course.course_id,
-												},
-												{
-													headers: {
-														"Content-Type":
-															"application/json",
-													},
-												}
-											)
-											.then((res) => {
-												alert(res.data.message);
-											});
-									}}
-									className="DropBtn"
-								>
-									Drop
-								</button>
-								<button
-									onClick={() => {}}
-									className="GoToCourseBtn"
-								>
-									Go to Course
-								</button>
-							</div>
+							<h2>{course.name}</h2>
 						</div>
-					))}
-				</div>
-				<div className="invites">
+						<div className="ButtonsDiv">
+							<button
+								onClick={() => {
+									axios
+										.post(
+											"http://localhost/react-elearning/server/enrollcourse.php",
+											{
+												jwt: localStorage.getItem(
+													"jwt"
+												),
+												course_id: course.course_id,
+											},
+											{
+												headers: {
+													"Content-Type":
+														"application/json",
+												},
+											}
+										)
+										.then((res) => {
+											alert(res.data.message);
+										});
+								}}
+								className="EnrollBtn"
+							>
+								Enroll
+							</button>
+							<button
+								onClick={() => {
+									axios
+										.post(
+											"http://localhost/react-elearning/server/dropcourse.php",
+											{
+												jwt: localStorage.getItem(
+													"jwt"
+												),
+												course_id: course.course_id,
+											},
+											{
+												headers: {
+													"Content-Type":
+														"application/json",
+												},
+											}
+										)
+										.then((res) => {
+											alert(res.data.message);
+										});
+								}}
+								className="DropBtn"
+							>
+								Drop
+							</button>
+							<button
+								onClick={() => {
+									localStorage.setItem("course", JSON.stringify(course))
+									navigate('/course')
+								}}
+								className="GoToCourseBtn"
+							>
+								Go to Course
+							</button>
+						</div>
+					</div>
+				))}
+			</div>
+			<div className="invites">
 				<h1>invites</h1>
 				{invites.map((course) => (
-						<div key={course.id} className="course">
-							<div className="CourseInfo">
-								<div className="icon">
-									<h3>{course.name[0]}</h3>
-								</div>
-								<h2>{course.name}</h2>
+					<div key={course.id} className="course">
+						<div className="CourseInfo">
+							<div className="icon">
+								<h3>{course.name[0]}</h3>
 							</div>
-							<div className="ButtonsDiv">
-								<button
-									onClick={() => {
-										axios
-											.post(
-												"http://localhost/react-elearning/server/acceptinvite.php",
-												{
-													jwt: localStorage.getItem(
-														"jwt"
-													),
-													course_id: course.course_id,
-												},
-												{
-													headers: {
-														"Content-Type":
-															"application/json",
-													},
-												}
-											)
-											.then((res) => {
-												alert(res.data.message);
-												fetchinvites();
-											});
-									}}
-									className="EnrollBtn"
-								>
-									Accept
-								</button>
-								<button
-									onClick={() => {
-										axios
-											.post(
-												"http://localhost/react-elearning/server/rejectinvite.php",
-												{
-													jwt: localStorage.getItem(
-														"jwt"
-													),
-													course_id: course.course_id,
-												},
-												{
-													headers: {
-														"Content-Type":
-															"application/json",
-													},
-												}
-											)
-											.then((res) => {
-												alert(res.data.message);
-												fetchinvites();
-											});
-									}}
-									className="DropBtn"
-								>
-									Reject
-								</button>
-							</div>
+							<h2>{course.name}</h2>
 						</div>
-					))}
-				</div>
+						<div className="ButtonsDiv">
+							<button
+								onClick={() => {
+									axios
+										.post(
+											"http://localhost/react-elearning/server/acceptinvite.php",
+											{
+												jwt: localStorage.getItem(
+													"jwt"
+												),
+												course_id: course.course_id,
+											},
+											{
+												headers: {
+													"Content-Type":
+														"application/json",
+												},
+											}
+										)
+										.then((res) => {
+											alert(res.data.message);
+											fetchinvites();
+										});
+								}}
+								className="EnrollBtn"
+							>
+								Accept
+							</button>
+							<button
+								onClick={() => {
+									axios
+										.post(
+											"http://localhost/react-elearning/server/rejectinvite.php",
+											{
+												jwt: localStorage.getItem(
+													"jwt"
+												),
+												course_id: course.course_id,
+											},
+											{
+												headers: {
+													"Content-Type":
+														"application/json",
+												},
+											}
+										)
+										.then((res) => {
+											alert(res.data.message);
+											fetchinvites();
+										});
+								}}
+								className="DropBtn"
+							>
+								Reject
+							</button>
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
